f007e907c1064e3857f35471ab47ac0aeb8d9858
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2021-08-31 10:23:56 +0300

    now we can delete tasks

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteTask
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task/<int:pk>', TaskDetail.as_view(),name='task'), 
     path('task-update/<int:pk>', TaskUpdate.as_view(),name='task-update'), 
     path('create', TaskCreate.as_view(),name='task-create'),
+    path('task-delete/<int:pk>', DeleteTask.as_view(),name='task-delete'),
 ]
