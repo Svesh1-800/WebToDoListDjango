40efeb55a3619501d83e88d1cda40db591a93f58
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2021-09-02 12:28:03 +0300

    user specific data

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -18,6 +18,12 @@ class TaskList(LoginRequiredMixin,ListView):
     model = Task
     context_object_name = "tasks"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+
 class TaskDetail(LoginRequiredMixin,DetailView):
     model = Task
     context_object_name = "task"
@@ -25,8 +31,18 @@ class TaskDetail(LoginRequiredMixin,DetailView):
 
 class TaskCreate(LoginRequiredMixin,CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title','description','complete']
     success_url =  reverse_lazy('tasks')
+
+    def form_valid(self,form):
+        form.cleaned_data['title'] = 'zopa'
+        print('---------------------------')
+        print(form.instance)
+        print('---------------------------')
+        print(form.instance.user)
+        print('---------------------------')
+        form.instance.user = self.request.user
+        return super(TaskCreate,self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
