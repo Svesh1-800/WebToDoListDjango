eb5b5ece5319fcfe651e81b0096aa41bac39b614
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2021-09-05 17:43:47 +0300

    structured folders and files

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteTask, CustomLoginView, LogoutView, RegisterPage
+from .views import TaskList, TaskCreate, TaskUpdate, DeleteTask, CustomLoginView, LogoutView, RegisterPage
 
 urlpatterns = [
     path('login/',CustomLoginView.as_view(),name="login"),
     path('register/',RegisterPage.as_view(),name="register"),
     path('logout/',LogoutView.as_view(next_page="login"),name="logout"),
-    path('', TaskList.as_view(),name='tasks'),
-    path('task/<int:pk>', TaskDetail.as_view(),name='task'), 
+    path('', TaskList.as_view(),name='tasks'), 
     path('task-update/<int:pk>', TaskUpdate.as_view(),name='task-update'), 
     path('create', TaskCreate.as_view(),name='task-create'),
     path('task-delete/<int:pk>', DeleteTask.as_view(),name='task-delete'),
